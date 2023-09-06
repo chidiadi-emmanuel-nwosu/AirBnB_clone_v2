2507a9c7b1bb73190a1c9cd1398f45b57975b76c
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2023-09-06 22:52:36 +0100

    refactored code

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -13,20 +13,17 @@ def do_deploy(archive_path):
     if not os.path.exists(archive_path):
         return False
 
-    name = archive_path.split('/')[-1]
-    folder = name.split('.')[0]
-
     try:
+        archive_name = archive_path.split('/')[-1]
+        release_folder = f'/data/web_static/releases/{name.split(".")[0]}'
+
         put(archive_path, '/tmp/')
-        run(f'mkdir -p /data/web_static/releases/{folder}')
-        run(f'tar -xzf /tmp/{name} -C /data/web_static/releases/{folder}')
-        run(f'rm /tmp/{name}')
-        run(f'mv /data/web_static/releases/{folder}/web_static/* '
-            f'/data/web_static/releases/{folder}/')
+        run(f'mkdir -p {release_folder}')
+        run(f'tar -xzf /tmp/{archive_name} -C {release_folder}')
+        run(f'rm /tmp/{archive_name}')
+        run(f'mv {release_folder}/web_static/* {release_folder}/')
         run(f'rm -rf /data/web_static/releases/{folder}/web_static/')
-        run(f'rm -rf /data/web_static/current')
-        run(f'ln -s /data/web_static/releases/{folder} '
-            '/data/web_static/current')
+        run(f'ln -sf {release_folder} /data/web_static/current')
         print('New version deployed!')
         return True
     except Exception:
