cbe48dd02dfcf35ba47a13480e5229c4119a2600
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2023-09-08 09:32:37 +0100

    fixed error with variales

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -15,14 +15,14 @@ def do_deploy(archive_path):
 
     try:
         archive_name = archive_path.split('/')[-1]
-        release_folder = f'/data/web_static/releases/{name.split(".")[0]}'
+        release_folder = f'/data/web_static/releases/{archive_name.split(".")[0]}'
 
         put(archive_path, '/tmp/')
         run(f'mkdir -p {release_folder}')
         run(f'tar -xzf /tmp/{archive_name} -C {release_folder}')
         run(f'rm /tmp/{archive_name}')
         run(f'mv {release_folder}/web_static/* {release_folder}/')
-        run(f'rm -rf /data/web_static/releases/{folder}/web_static/')
+        run(f'rm -rf /data/web_static/releases/{release_folder}/web_static/')
         run(f'rm -rf /data/web_static/current')
         run(f'ln -s {release_folder} /data/web_static/current')
         print('New version deployed!')
