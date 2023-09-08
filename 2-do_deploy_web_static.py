d6da58b6700596ebb6a3de573d18408cadb0d60e
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2023-09-08 09:37:10 +0100

    fixed style error

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -14,13 +14,13 @@ def do_deploy(archive_path):
         return False
 
     try:
-        archive_name = archive_path.split('/')[-1]
-        release_folder = f'/data/web_static/releases/{archive_name.split(".")[0]}'
+        arc_name = archive_path.split('/')[-1]
+        release_folder = f'/data/web_static/releases/{arc_name.split(".")[0]}'
 
         put(archive_path, '/tmp/')
         run(f'mkdir -p {release_folder}')
-        run(f'tar -xzf /tmp/{archive_name} -C {release_folder}')
-        run(f'rm /tmp/{archive_name}')
+        run(f'tar -xzf /tmp/{arc_name} -C {release_folder}')
+        run(f'rm /tmp/{arc_name}')
         run(f'mv {release_folder}/web_static/* {release_folder}/')
         run(f'rm -rf /data/web_static/releases/{release_folder}/web_static/')
         run(f'rm -rf /data/web_static/current')
