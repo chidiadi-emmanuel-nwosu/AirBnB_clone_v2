86713adf2e4cfc19019473ab65850f5b7703551b
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2023-09-08 09:40:17 +0100

    fixed error

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -30,15 +30,15 @@ def do_deploy(archive_path):
         return False
 
     try:
-        archive_name = archive_path.split('/')[-1]
-        release_folder = f'/data/web_static/releases/{name.split(".")[0]}'
+        arc_name = archive_path.split('/')[-1]
+        release_folder = f'/data/web_static/releases/{arc_name.split(".")[0]}'
 
         put(archive_path, '/tmp/')
         run(f'mkdir -p {release_folder}')
-        run(f'tar -xzf /tmp/{archive_name} -C {release_folder}')
-        run(f'rm /tmp/{archive_name}')
+        run(f'tar -xzf /tmp/{arc_name} -C {release_folder}')
+        run(f'rm /tmp/{arc_name}')
         run(f'mv {release_folder}/web_static/* {release_folder}/')
-        run(f'rm -rf /data/web_static/releases/{folder}/web_static/')
+        run(f'rm -rf /data/web_static/releases/{release_folder}/web_static/')
         run(f'ln -sf {release_folder} /data/web_static/current')
         print('New version deployed!')
         return True
