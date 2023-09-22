6af298cff861254cc7f7d91fc45e11cef7433a6c
chidiadi-emmanuel-nwosu <chidiadi.emmanuel.nwosu@gmail.com>
2023-09-22 19:52:01 +0100

    refactored code again for task 7

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -9,12 +9,6 @@ from models.state import State
 app = Flask("__main__")
 
 
-@app.teardown_appcontext
-def teardown(exception):
-    """calls in the storage close method"""
-    storage.close()
-
-
 @app.route('/states_list', strict_slashes=False)
 def list_states():
     """return all states"""
@@ -22,5 +16,11 @@ def list_states():
     return render_template('7-states_list.html', states=states)
 
 
+@app.teardown_appcontext
+def teardown(exception):
+    """calls in the storage close method"""
+    storage.close()
+
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
